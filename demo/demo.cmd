956064331148db62c24cc0a481caed81b481070e
PaxTibi <marcoroin@gmail.com>
2020-01-14 18:20:17 +0100

    Update demo.cmd

--- demo/demo.cmd
+++ demo/demo.cmd
@@ -1,1 +1,1 @@
-..\bin\i386-win32\tmplGen.exe -p .\configs\bootstrap-plugin.json -f "saluta."  class-name=Saluto plugin-name=saluta version=1.0.0 selector=["role"="ciao-Cristian"] template-selector="role"="ciao-Cristian" targetpath=.
+..\bin\i386-win32\tmplGen.exe -p .\configs\bootstrap-plugin.json -f "hello."  class-name=Saluto plugin-name=saluta version=1.0.0 selector=["role"="hello-guy"] template-selector="role"="hello-guy" targetpath=.
