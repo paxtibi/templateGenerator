49bbc06ecd7ba8f6dfb268cd1dfd85f26174add0
PaxTibi <marcoroin@gmail.com>
2020-03-22 23:05:34 +0100

    fix template example

--- demo/templates/pascal/numerical/Json.pas
+++ demo/templates/pascal/numerical/Json.pas
@@ -21,7 +21,7 @@ var
   value : I<%type-name%>;
 begin
   Result := False;
-  if (info^.PropType^.Kind = tkInterface) and  (info^.PropType^.Name = 'IDouble') and (node <> nil) then
+  if (info^.PropType^.Kind = tkInterface) and  (info^.PropType^.Name = 'I<%type-name%>') and (node <> nil) then
   begin
     try
       value := GetFloatProp(AObject, Info);
